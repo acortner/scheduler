ab9f57dc92cd9d468268479311a24c3fb4551e15
Andrew Cortner <andrewcortner2021@u.northwestern.edu>
2020-01-16 15:19:41 -0600

    slice_6

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import 'rbx/index.css';
 import { Button, Container, Title } from 'rbx';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCmPm4fFHMvbsVjEX-9huWv4ESh6PlpuTs",
+  authDomain: "course-scheduler-9fb29.firebaseapp.com",
+  databaseURL: "https://course-scheduler-9fb29.firebaseio.com",
+  projectId: "course-scheduler-9fb29",
+  storageBucket: "course-scheduler-9fb29.appspot.com",
+  messagingSenderId: "317241018111",
+  appId: "1:317241018111:web:90ec15c2f928e2c51faeca"
+};
+
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
 const days = ["M", "Tu", "W", "Th", "F"];
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": [
-    {
-      "id": "F101",
-      "title": "Computer Science: Concepts, Philosophy, and Connections",
-      "meets": "MWF 11:00-11:50"
-    },
-    {
-      "id": "F110",
-      "title": "Intro Programming for non-majors",
-      "meets": "MWF 10:00-10:50"
-    },
-    {
-      "id": "F111",
-      "title": "Fundamentals of Computer Programming I",
-      "meets": "MWF 13:00-13:50"
-    },
-    {
-      "id": "F211",
-      "title": "Fundamentals of Computer Programming II",
-      "meets": "TuTh 12:30-13:50"
-    }
-  ]
-};
 
 const Banner = ({ title }) => (
   <Title>{ title || '[loading...]' }</Title>
@@ -78,8 +66,6 @@ const TermSelector = ({ state }) => (
   </Button.Group>
 );
 
-const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
-
 const getCourseTerm = course => (
   terms[course.id.charAt(0)]
 );
@@ -90,16 +76,30 @@ const getCourseNumber = course => (
   
 const Course = ({ course, state }) => (
   <Button color={ buttonColor(state.selected.includes(course)) }
-    onClick={ () => state.toggle(course) }
-    disabled={ hasConflict(course, state.selected) }
-    >
-    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
-  </Button>
-);
+      onClick={ () => state.toggle(course) }
+      onDoubleClick={ () => moveCourse(course) }
+      disabled={ hasConflict(course, state.selected) }
+      >
+      { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+    </Button>
+  );
 
 const hasConflict = (course, selected) => (
   selected.some(selection => courseConflict(course, selection))
 );
+
+const moveCourse = course => {
+  const meets = prompt('Enter new meeting data, in this format:', course.meets);
+  if (!meets) return;
+  const {days} = timeParts(meets);
+  if (days) saveCourse(course, meets); 
+  else moveCourse(course);
+};
+
+const saveCourse = (course, meets) => {
+  db.child('courses').child(course.id).update({meets})
+    .catch(error => alert(error));
+};
 
 const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
 
@@ -121,7 +121,7 @@ const addCourseTimes = course => ({
 
 const addScheduleTimes = schedule => ({
   title: schedule.title,
-  courses: schedule.courses.map(addCourseTimes)
+  courses: Object.values(schedule.courses).map(addCourseTimes)
 });
 
 const daysOverlap = (days1, days2) => ( 
@@ -144,24 +144,24 @@ const courseConflict = (course1, course2) => (
 
 const App = () => {
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
 
   useEffect(() => {
-    const fetchSchedule = async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(addScheduleTimes(json));
+    const handleData = snap => {
+      if (snap.val()) setSchedule(addScheduleTimes(snap.val()));
     }
-    fetchSchedule();
-  }, [])
+    db.on('value', handleData, error => alert(error));
+    return () => { db.off('value', handleData); };
+  }, []);
 
   return (
     <Container>
       <Banner title={ schedule.title } />
       <CourseList courses={ schedule.courses } />
     </Container>
-  );
+);
 };
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database().ref();
 
 export default App;
